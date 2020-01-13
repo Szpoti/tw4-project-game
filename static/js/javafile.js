4da9fb20ada26bb60ca9acc32087543868d1d6ba
Faragó Balázs <balage.farago@gmail.com>
2020-01-13 14:14:20 +0100

    test 6

--- static/js/javafile.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("app").innerHTML = `
-<p> Hello Again, this is javascript</p>
-<p>new branch test</p>
-<p>test-szpoti</p>
-<p> Noémi! <p>
-<p>szpoti dev</p>
-<p>szpoti dev2</p>
-`;
