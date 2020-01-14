6089b1df670f84523ba53d4dc028394611a46310
Kincso Osz-Farkas <ofkincso99@gmail.com>
2020-01-14 14:54:44 +0100

    refactored betting.js

--- static/js/betting.js
+++ static/js/betting.js
@@ -1,44 +1,25 @@
-let my_bet=0;
-let my_bank=1000;
+let my_bet = 0;
+let my_bank = 1000;
 console.log(my_bet);
 console.log(my_bank);
 let bet10 = document.getElementById('btn10');
 let bet20 = document.getElementById('btn20');
 let bet50 = document.getElementById('btn50');
 let bet100 = document.getElementById('btn100');
-bet10.addEventListener('click', function () {
-   if (my_bet >= my_bank) {
-    document.getElementsByClassName('tokens').disabled = true}
-    else{
-        my_bet+=10;
-        display.textContent=my_bet}
-});
-bet20.addEventListener('click', function () {
-    if (my_bet >= my_bank) {
-    document.getElementsByClassName('tokens').disabled = true}
-    else if (my_bet+20>my_bank){
-            document.getElementsByClassName('tokens').disabled = true}
-    else{
-        my_bet+=20;
-        display.textContent=my_bet}
-});
-bet50.addEventListener('click', function () {
-    if (my_bet >= my_bank) {
-    document.getElementsByClassName('tokens').disabled = true}
-    else if (my_bet+50>my_bank){
-            document.getElementsByClassName('tokens').disabled = true}
-    else{
-        my_bet+=50;
-        display.textContent=my_bet}
-});
-bet100.addEventListener('click', function () {
-    if (my_bet >= my_bank) {
-    document.getElementsByClassName('tokens').disabled = true}
-    else if (my_bet+100>my_bank){
-            document.getElementsByClassName('tokens').disabled = true}
-    else{
-        my_bet+=100;
-        display.textContent=my_bet}
 
-});
+function myBets(betName, betAmount) {
+    betName.addEventListener('click', function () {
+        if (my_bet >= my_bank) {
+        document.getElementsByClassName('tokens').disabled = true}
+        else if (my_bet + betAmount > my_bank){
+            document.getElementsByClassName('tokens').disabled = true}
+        else{
+            my_bet += betAmount;
+            display.textContent = my_bet}
+    });
+}
 
+myBets(bet10, 10);
+myBets(bet20, 20);
+myBets(bet50, 50);
+myBets(bet100, 100);
