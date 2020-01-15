6ea0b488b8321e1e4fc2842b46bf2d7a98c9ae0a
Faragó Balázs <balage.farago@gmail.com>
2020-01-15 10:59:00 +0100

    Into the deck function implemented, playable deck is created

--- static/js/javafile.js
+++ static/js/javafile.js
@@ -2,11 +2,12 @@ let xCord = 50, yCord = 100;
 let xPos = 0, yPos = 0;
 let cards = {};
 let cardsId = [];
-
+let cnt = 1;
+let rnd = 0;
 for (row = 0; row < 4; row++) {
     for (column = 0; column < 13; column++) {
         xPos += 69;
-        xCord += 80;
+        xCord += 50;
 
 
         let data_value = xPos / 69;
@@ -17,18 +18,18 @@ for (row = 0; row < 4; row++) {
         if(data_value === 1)
         { data_value = 11;}
 
-        const card = `<div onclick="this.classList.toggle('flipped')" class="card" id="card-${row}-${column}" data-value="${data_value}" style="
+        const card = `<div onclick="this.classList.toggle('flipped')" class="card" id="card-${row}-${column}" style="
         border: black solid 2px;
+        z-index: ${cnt};
         border-radius: 10px;
         position: absolute;
         cursor: pointer;
         top: ${yCord}px;
         left: ${xCord}px;
     ">
-    <div class="side front" style="background-position: -${xPos}px ${yPos}px"></div>
-    <div class="side back" style="background-position: 0px 0px;"></div>
+    <div class="side front" data-value="${data_value}" style="background-position: -${xPos}px ${yPos}px"></div>
+    <div class="side back"  data-value="${data_value}" style="background-position: 0px 0px;"></div>
 </div>`;
-
         let key = `card-${row}-${column}`;
         cards[key] = `-${xPos}px ${yPos}px`;
         cardsId.push(key);
@@ -45,10 +46,32 @@ function stackShuffle(cardsId) {
     while (count) {
         cardsId.push(cardsId.splice(Math.floor(Math.random() * count), 1)[0]);
         count -= 1;
-    }
 
+    }
+    console.log(cardsId);
     displayCards(cardsId);
 }
+
+function flipCards(cardsId) {
+    console.log(cardsId);
+    for (card of cardsId) {
+        document.getElementById(card).classList.toggle('flipped')
+    }
+}
+
+function intoDeck(cardsId) {
+        console.log(cardsId);
+        let xCord = 500, yCord = 250;
+        let count = 52;
+        for (id of cardsId) {
+            rnd = Math.floor(Math.random() * cardsId.length);
+            let card = document.getElementById(id);
+            card.style.top = `${yCord}px`;
+            card.style.left = `${xCord}px`;
+            card.style.zIndex = `${count}`;
+            count -= 1;
+        }
+    }
 
 function displayCards(cardsId) {
     let xCord = 130, yCord = 100;
@@ -57,7 +80,7 @@ function displayCards(cardsId) {
         let card = document.getElementById(`${id}`);
         card.style.top = `${yCord}px`;
         card.style.left = `${xCord}px`;
-        xCord += 80;
+        xCord += 50;
         count += 1;
         if (count === 13) {
             xCord = 130;
@@ -66,3 +89,13 @@ function displayCards(cardsId) {
         }
         }
     }
+
+allCards = document.querySelectorAll('.card');
+for (card of allCards) {
+    card.addEventListener('click', alertValue);
+}
+
+function alertValue(event) {
+    let value = event.target.dataset.value;
+    alert(`${value}`);
+}
