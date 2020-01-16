c99a006d7eee77b4d718e13d0b01758acb85be87
Faragó Balázs <balage.farago@gmail.com>
2020-01-16 18:30:35 +0100

    Finalizeing, Ace handling

--- static/js/javafile.js
+++ static/js/javafile.js
@@ -7,12 +7,16 @@ let cards = {};
 let cardsId = [];
 let deck = [];
 let hand = []; let dealerHand = [];
+let valuesInHand = [];
 let cnt = 1;
 let rnd = 0;
 let handCordX = 55;
 let dealerCordX = 55;
 let handZIndex = 100;
-let handValue = 0; let dealerHandValue = 0 ;
+let handValue = 0; let dealerHandValue = 0;
+let bankDisplay = document.getElementById('bank');
+let betDisplay = document.getElementById('display');
+let aces = 0; let dealerAces = 0;
 for (row = 0; row < 4; row++) {
     for (column = 0; column < 13; column++) {
         xPos += 7.69;
@@ -43,6 +47,7 @@ for (row = 0; row < 4; row++) {
         cardsId.push(key);
         deck.push(key);
         document.getElementById('card-table').insertAdjacentHTML('beforeend', card);
+        document.getElementsByClassName('welcome')
     }
     xPos = 0;
     yPos += 33.3;
@@ -96,7 +101,7 @@ function displayCards(cardsId) {
             xCord = 40;
             yCord += 140;
             count = 0;
-        }
+            }
         }
     }
 
@@ -142,6 +147,7 @@ function playerDraw() {
 
 function dealerDraw(cnt) {
     let topCard = document.getElementById(`${cardsId[0]}`);
+    let value = Math.ceil(parseFloat(topCard.dataset.value));
     dealerHand.push(topCard.id);
     cardsId.splice(topCard.id, 1);
     if (cnt === 1) {
@@ -150,9 +156,22 @@ function dealerDraw(cnt) {
     topCard.style.top = `13.5vh`;
     topCard.style.left = `${dealerCordX}vh`;
     topCard.style.zIndex = `${handZIndex}`;
+    if (value === 11){
+            dealerAces += 1;
+            if (dealerHandValue + value > 21){
+                value -= 10;
+                dealerAces -= 1;
+            }
+        }
+        if (dealerHandValue + value > 21 && dealerAces > 0){
+            dealerHandValue -= 10;
+            dealerHandValue += value;
+            dealerAces -= 1;
+        } else {
+            dealerHandValue += value;
+        }
     handZIndex += 1;
     dealerCordX += 13;
-    dealerHandValue += Math.ceil(parseFloat(topCard.dataset.value));
     topCard.removeEventListener('click', drawFromDeck);
 }
 
@@ -161,6 +180,7 @@ function drawFromDeck(event) {
     // alert(event.target.className);
     let parent = event.target.parentNode;
     if (parent.id !== 'card-table') {
+        let value = Math.ceil(parseFloat(parent.dataset.value));
         hand.push(parent.id);
         //console.log(cardsId);
         cardsId.splice(parent.id, 1);
@@ -170,10 +190,22 @@ function drawFromDeck(event) {
         parent.style.zIndex = `${handZIndex}`;
         handZIndex += 1;
         handCordX += 13;
-        handValue += Math.ceil(parseFloat(parent.dataset.value));
+        if (value === 11){
+            aces += 1;
+            if (handValue + value > 21){
+                value -= 10;
+                aces -= 1;
+            }
+        }
+        if (handValue + value > 21 && aces > 0){
+            handValue -= 10;
+            handValue += value;
+            aces -= 1;
+        } else {
+            handValue += value;
+        }
         parent.removeEventListener('click',drawFromDeck);
         checkForLose(handValue);
-
     } else {
         event.target.classList.toggle('flipped');
     }
@@ -188,34 +220,26 @@ function flipDown(cards) {
     }
 }
 
-function standButton() {
-    checkForLose(handValue);
-    if (handValue < 21) {
-        alert("Your score is: " + handValue)
+async function standButton() {
+    document.getElementById(`${dealerHand[0]}`).classList.toggle('flipped');
+    while (status === null) {
+        await sleep(1000);
+        if (dealerHandValue >= 17) {
+            status = checkForWin();
+        } else {
+            dealerDraw(cnt = 1);
+        }
     }
-}
-
-async function checkForLose(handValue) {
-    if (handValue > 21) {
-        await sleep(500);
-        alert('Sorry, you lost.\n Your hand\'s value is bigger than 21.');
-        status = false;
-        dealPrize();
-        buttonReset();
-    }
-    else if (handValue === 21) {
-        await sleep(500);
-        alert('You WON!');
-    }
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+    dealPrize(status);
+    buttonReset();
+    betDisplay.textContent=`Place your bets.`;
+    bankDisplay.textContent=`Bank: ${my_bank}$`;
 }
 
 async function newGame() {
-
     if (my_bet > 0) {
+        status = null;
+        aces = 0;
         let tokens = document.querySelectorAll('.tokens');
         for (token of tokens){
             token.removeEventListener('click', tokenHandler)
@@ -236,35 +260,42 @@ async function newGame() {
 
 function tokenHandler(event) {
         if (event.target.id === 'reset') {
+            my_bank += my_bet;
             my_bet = 0;
         }
         if (my_bet >= my_bank) { console.log('off') }
         else {
             if (event.target.id === 'btn10') {
+                my_bank -= 10;
                 my_bet += 10;
             }
             else if (event.target.id === 'btn20') {
                 if (my_bet+20>my_bank){console.log('off')}
                 else {
+                    my_bank -= 20;
                     my_bet += 20;
                 }
             }
             else if (event.target.id === 'btn50') {
                 if (my_bet+50>my_bank){console.log('off')}
                 else {
+                    my_bank -= 50;
                     my_bet += 50;
                 }
             }
             else if (event.target.id === 'btn100') {
                 if (my_bet+100>my_bank){console.log('off')}
                 else {
+                    my_bank -= 100;
                     my_bet += 100;
                 }
             }
             else if (event.target.id === 'all_in') {
-                my_bet = my_bank;
+                my_bet += my_bank;
+                my_bank = 0;
                 }
             display.textContent=`Your bet is ${my_bet}$`;
+            bankDisplay.textContent=`Bank: ${my_bank}$`
         }
 }
 
@@ -275,14 +306,58 @@ function buttonReset() {
     }
 }
 
-function dealPrize(){
+function dealPrize(status){
     if (status === false) {
-        my_bank -= my_bet;
+        alert('Sorry, you lost!');
+        my_bet = 0;
     } else if (status === true) {
-        my_bank = my_bet*2;
+        alert('You win!');
+        my_bank += my_bet*2;
+        my_bet = 0;
+    } else {
+        alert('Draw')
+        my_bank += my_bet;
+        my_bet = 0;
+    }
+    bankDisplay.textContent=`Bank: ${my_bank}$`;
+    if (my_bank === 0) {alert ('Sorry, you are bankrupt...RIP')}
+}
+
+function checkForWin(){
+    if (dealerHandValue > 21) {
+        return true;
+    }
+    if (handValue > dealerHandValue){
+        return true;
+    } else if (handValue < dealerHandValue){
+        return false;
+    } else {
+        if (hand.length < dealerHand.length){
+            return true;
+        } else if (hand.length > dealerHand.length){
+            return false;
+        } else {
+            return 'draw';
+        }
     }
 }
 
-window.onload = function () {flipCards(cardsId); intoDeck(cardsId); buttonReset()};
+async function checkForLose(handValue) {
+    if (handValue > 21) {
+        await sleep(500);
+        alert('Your hand\'s value is bigger than 21.');
+        dealPrize(status = false);
+        buttonReset();
+        status = null;
+        display.textContent=`Place your bets.`;
+
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+window.onload = function () {flipCards(cardsId); intoDeck(cardsId); buttonReset(); bankDisplay.textContent=`Bank: ${my_bank}$`};
 
 
