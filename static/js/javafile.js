758b54157f6f451fb4c955b01df5f955deaebd07
Faragó Balázs <balage.farago@gmail.com>
2020-01-16 16:36:52 +0100

    final touches, lose check, prizes

--- static/js/javafile.js
+++ static/js/javafile.js
@@ -1,6 +1,6 @@
 let my_bet = 0;
 let my_bank = 1000;
-
+let status = null;
 let xCord = 40, yCord = 100;
 let xPos = 0, yPos = 0;
 let cards = {};
@@ -9,7 +9,7 @@ let deck = [];
 let hand = []; let dealerHand = [];
 let cnt = 1;
 let rnd = 0;
-let handCordX = 450;
+let handCordX = 55;
 let dealerCordX = 55;
 let handZIndex = 100;
 let handValue = 0; let dealerHandValue = 0 ;
@@ -72,7 +72,13 @@ function displayCards(cardsId) {
     for (card of hand) {
         cardsId.push(card);
     }
-    handCordX = 450;
+    for (card of dealerHand) {
+        cardsId.push(card);
+    }
+    dealerCordX = 55;
+    dealerHandValue = 0;
+    dealerHand = [];
+    handCordX = 55;
     handValue = 0;
     hand = [];
     xCord = 40; yCord = 100;
@@ -96,25 +102,51 @@ function displayCards(cardsId) {
 
 function intoDeck(cardsId) {
     console.log(cardsId);
-    let xCord = 40, yCord = 390;
+    let xCord = 9.5, yCord = 32;
     let count = 52;
     for (id of cardsId) {
         rnd = Math.floor(Math.random() * cardsId.length);
         let card = document.getElementById(id);
         card.addEventListener('click', drawFromDeck);
         card.setAttribute('onclick',"this.classList.toggle('flipped')");
-        card.style.top = `${yCord}px`;
-        card.style.left = `${xCord}px`;
+        card.style.top = `${yCord}vh`;
+        card.style.left = `${xCord}vh`;
         card.style.zIndex = `${count}`;
         count -= 1;
-        xCord += 0.1;
+        xCord += 0.01;
     }
 }
 
-function dealerDraw() {
+async function dealStartingHand(){
+    for (i=0;i <= 1;i++) {
+        dealerDraw(i);
+        await sleep(200);
+        playerDraw();
+        await sleep(200);
+    }
+}
+
+function playerDraw() {
+    let topCard = document.getElementById(`${cardsId[0]}`);
+    hand.push(topCard.id);
+    cardsId.splice(topCard.id, 1);
+    topCard.classList.toggle('flipped');
+    topCard.style.top = `55.5vh`;
+    topCard.style.left = `${handCordX}vh`;
+    topCard.style.zIndex = `${handZIndex}`;
+    handZIndex += 1;
+    handCordX += 13;
+    handValue += Math.ceil(parseFloat(topCard.dataset.value));
+    topCard.removeEventListener('click', drawFromDeck);
+}
+
+function dealerDraw(cnt) {
     let topCard = document.getElementById(`${cardsId[0]}`);
     dealerHand.push(topCard.id);
     cardsId.splice(topCard.id, 1);
+    if (cnt === 1) {
+        topCard.classList.toggle('flipped');
+    }
     topCard.style.top = `13.5vh`;
     topCard.style.left = `${dealerCordX}vh`;
     topCard.style.zIndex = `${handZIndex}`;
@@ -122,7 +154,6 @@ function dealerDraw() {
     dealerCordX += 13;
     dealerHandValue += Math.ceil(parseFloat(topCard.dataset.value));
     topCard.removeEventListener('click', drawFromDeck);
-    checkForLose(dealerHandValue);
 }
 
 function drawFromDeck(event) {
@@ -134,18 +165,27 @@ function drawFromDeck(event) {
         //console.log(cardsId);
         cardsId.splice(parent.id, 1);
         //console.log(cardsId);
-        parent.style.left = `${handCordX}px`;
-        parent.style.top = '645px';
+        parent.style.left = `${handCordX}vh`;
+        parent.style.top = '55.5vh';
         parent.style.zIndex = `${handZIndex}`;
         handZIndex += 1;
-        handCordX += 120;
+        handCordX += 13;
         handValue += Math.ceil(parseFloat(parent.dataset.value));
         parent.removeEventListener('click',drawFromDeck);
+        checkForLose(handValue);
 
     } else {
         event.target.classList.toggle('flipped');
     }
-    checkForLose(handValue);
+}
+
+function flipDown(cards) {
+    for (id of cards) {
+        let card = document.getElementById(`${id}`);
+        if (card.className !== 'card flipped') {
+            card.classList.toggle('flipped')
+        }
+    }
 }
 
 function standButton() {
@@ -158,7 +198,10 @@ function standButton() {
 async function checkForLose(handValue) {
     if (handValue > 21) {
         await sleep(500);
-        alert('Sorry, you lost.\n Your hand\'s value is bigger than 21.')
+        alert('Sorry, you lost.\n Your hand\'s value is bigger than 21.');
+        status = false;
+        dealPrize();
+        buttonReset();
     }
     else if (handValue === 21) {
         await sleep(500);
@@ -170,35 +213,28 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-async function startUpFunctions() {
-    await sleep(500);
-    flipCards(cardsId);
-    await sleep(500);
-    stackShuffle(cardsId);
-    await sleep(500);
-    intoDeck(cardsId);
-    await sleep(500);
-    dealerDraw();
-    await sleep(100);
-    dealerDraw();
-    await sleep(100);
-}
-
 async function newGame() {
-    await sleep(500);
-    flipCards(hand);
-    await sleep(500);
-    displayCards(cardsId);
-    await sleep(500);
-    stackShuffle(cardsId);
-    await sleep(500);
-    intoDeck(cardsId);
+
+    if (my_bet > 0) {
+        let tokens = document.querySelectorAll('.tokens');
+        for (token of tokens){
+            token.removeEventListener('click', tokenHandler)
+        }
+        await sleep(100);
+        flipDown(hand);
+        flipDown(dealerHand);
+        await sleep(500);
+        displayCards(cardsId);
+        await sleep(500);
+        stackShuffle(cardsId);
+        await sleep(500);
+        intoDeck(cardsId);
+        await sleep(500);
+        dealStartingHand();
+    }
 }
 
-let tokens = document.querySelectorAll('.tokens');
-
-for (token of tokens) {
-    token.addEventListener('click', function (event) {
+function tokenHandler(event) {
         if (event.target.id === 'reset') {
             my_bet = 0;
         }
@@ -229,9 +265,24 @@ for (token of tokens) {
                 my_bet = my_bank;
                 }
             display.textContent=`Your bet is ${my_bet}$`;
-        }})}
+        }
+}
 
+function buttonReset() {
+    let tokens = document.querySelectorAll('.tokens');
+    for (token of tokens) {
+        token.addEventListener('click', tokenHandler)
+    }
+}
 
-window.onload = startUpFunctions();
+function dealPrize(){
+    if (status === false) {
+        my_bank -= my_bet;
+    } else if (status === true) {
+        my_bank = my_bet*2;
+    }
+}
+
+window.onload = function () {flipCards(cardsId); intoDeck(cardsId); buttonReset()};
 
 
